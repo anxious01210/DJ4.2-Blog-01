eaadd4f03023b7afecc8b05768d895e7298fcc03
anxious01210 <pinktheory21@gmail.com>
2023-04-15 20:07:07 +0300

    4th: blog_app CH2 finished. commenting sys is functional.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 def post_list(request):
@@ -15,6 +18,7 @@ def post_list(request):
         # If page_number is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
+        # If page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
@@ -28,4 +32,47 @@ def post_detail(request, year, month, day, post):
     # post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            print(request.build_absolute_uri(post.get_absolute_url()))
+            print(request.build_absolute_uri(post))
+            print(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} \n \n {cd['name']}\'s comments:  {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
